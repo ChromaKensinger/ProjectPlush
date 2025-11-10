ebb329fb92f87c5796d646b99012c754493dd5b9
SherynCherry <164809087+SherynCherry@users.noreply.github.com>
2025-11-09 19:52:52 -0800

    DragonPlush_Stand_Idle Animation
    Dragon & Idle animation is done!

---
+++ project-plush/dragon_plush_stand_idle.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+# Get a reference to the AnimatedSprite2D node.
+# The $ notation is a shortcut for get_node("AnimatedSprite2D").
+@onready var animated_sprite = $AnimatedSprite2D
+
+# This function is called once when the node (and its children)
+# enter the scene tree.
+func _ready():
+	# Tell the animated sprite to play the "DragonPlush_Stand_Idle" animation.
+	# Make sure the name in the quotes exactly matches your animation name.
+	animated_sprite.play("DragonPlush_Stand_Idle")
